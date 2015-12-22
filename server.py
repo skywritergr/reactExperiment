d3338682f44fa1242f6df9ca2763a5e88c993514
unknown <gstefani@SW-GBHA-SHEL311.emea.wdpr.disney.com>
2015-12-22 17:03:31 +0000

    Adding functionality in the server to retrieve all my retweets

--- server.py
+++ server.py
@@ -4,8 +4,13 @@ import time
 from urllib.request import Request, urlopen, URLError
 from flask import Flask, Response, request
 from requests_oauthlib import OAuth1Session
+from pprint import pprint
+from server.tweet import Tweet
 
 app = Flask(__name__, static_url_path='', static_folder='public')
+app.config.update(
+    PROPAGATE_EXCEPTIONS = True
+)
 app.add_url_rule('/', 'root', lambda: app.send_static_file('index.html'))
 
 @app.route('/api/comments', methods=['GET', 'POST'])
@@ -24,6 +29,13 @@ def comments_handler():
 
     return Response(json.dumps(comments), mimetype='application/json', headers={'Cache-Control': 'no-cache'})
     
+def prepareTweets(results):
+    tweetList = []
+    for res in results:
+        tempTweet = Tweet(res['text'], res['id'], res['retweet_count'], res['user']['name'])
+        tweetList.append(tempTweet)
+    return tweetList
+    
 @app.route('/api/getTweets', methods=['GET'])
 def get_tweets():        
     try:
@@ -36,11 +48,12 @@ def get_tweets():
                     resource_owner_key=keys['resource_owner_key'],
                     resource_owner_secret=keys['resource_owner_secret'])
         url = 'https://api.twitter.com/1.1/statuses/retweets_of_me.json'
-        res = twitter.get(url).json()
-
-        return Response(json.dumps(res), mimetype='application/json', headers={'Cache-Control': 'no-cache'})
+        res = twitter.get(url).json()  
     except URLError as e:
-       return Response(e)
+       return Response(e)   
+    else:
+        finalResult = prepareTweets(res)
+        return Response(json.dumps([ob.__dict__ for ob in finalResult]), mimetype='application/json', headers={'Cache-Control': 'no-cache'})
 
 if __name__ == '__main__':
     app.run(port=int(os.environ.get("PORT",3000)))
---
+++ server/tweet.py
@@ -0,0 +1,11 @@
+class Tweet(object):
+    text = ""
+    id = 0
+    retweet_count = 0
+    user_name = ""
+    
+    def __init__(self, text, id, retweet_count, user_name):
+        self.text = text
+        self.id = id
+        self.retweet_count = retweet_count
+        self.user_name = user_name
